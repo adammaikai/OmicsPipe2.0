b75934fd5b4522774f534f272d7e30e997013b01
adammaikai <adam.maikai@gmail.com>
2016-07-12 20:08:45 +0000

    updated main and utils

--- build/lib.linux-x86_64-2.7/omics_pipe/main.py
+++ build/lib.linux-x86_64-2.7/omics_pipe/main.py
@@ -60,131 +60,131 @@ def main():
             default_parameters.update(SAMPLE_LIST = sample_list2)   #Update default parameters to user defined parameter file
             p = Bunch(default_parameters)
      
-    check_create_dir(p.LOG_PATH)
-    check_create_dir(p.FLAG_PATH)
+    check_create_dir(p.OMICSPIPE["LOG_PATH"])
+    check_create_dir(p.OMICSPIPE["FLAG_PATH"])
     
     current_cwd = os.getcwd()
-    os.chdir(p.WORKING_DIR)
-    for x in os.listdir(p.WORKING_DIR):
+    os.chdir(p.OMICSPIPE["WORKING_DIR"])
+    for x in os.listdir(p.OMICSPIPE["WORKING_DIR"]):
         os.chmod(x,0755)
     
-    os.environ["DRMAA_LIBRARY_PATH"] = p.DRMAA_PATH 
+    os.environ["DRMAA_LIBRARY_PATH"] = p.OMICSPIPE["DRMAA_PATH"] 
     
     start_time = time.time()
     print start_time
 
-    decompress(p.RAW_DATA_DIR, args.compression)     #Check if files zipped, if so, unzip them   
+    #decompress(p.DATA["RAW_DATA"], args.compression)     #Check if files zipped, if so, unzip them   
 
-    record, project = sumatra_start(p.REPOSITORY, p.SUMATRA_DB_PATH, p.RESULTS_PATH, p.WORKING_DIR, p.HG_USERNAME, p.SUMATRA_RUN_NAME, parameters) #Create repo and sumatra project, start recording
+#    record, project = sumatra_start(p.SUMATRA["REPOSITORY"], p.SUMATRA["DB_PATH"], p.SUMATRA["RESULTS"], p.OMICSPIPE["WORKING_DIR"], p.SUMATRA["HG_USERNAME"], p.SUMATRA["RUN_NAME"], parameters) #Create repo and sumatra project, start recording
  
-    os.chdir(p.WORKING_DIR)
+    os.chdir(p.OMICSPIPE["WORKING_DIR"])
     
     if args.analysis_type == 'RNAseq_Tuxedo':
         runpy.run_module('omics_pipe.RNAseq_Tuxedo', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'RNAseq_count_based':
         runpy.run_module('omics_pipe.RNAseq_count_based', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'RNAseq_cancer_report':
         runpy.run_module('omics_pipe.RNAseq_cancer_report', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'RNAseq_TCGA_counts':
         runpy.run_module('omics_pipe.RNAseq_TCGA_counts', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'RNAseq_TCGA':
         runpy.run_module('omics_pipe.RNAseq_TCGA', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'Tumorseq_MUTECT':
         runpy.run_module('omics_pipe.Tumorseq_MUTECT', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'miRNAseq_count_based':
         runpy.run_module('omics_pipe.miRNAseq_count_based', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'miRNAseq_tuxedo':
         runpy.run_module('omics_pipe.miRNAseq_tuxedo', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'WES_GATK':
         runpy.run_module('omics_pipe.WES_GATK', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'WES_GATK_group_calling':
         runpy.run_module('omics_pipe.WES_GATK_group_calling', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'WGS_GATK':
         runpy.run_module('omics_pipe.WGS_GATK', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'WES_GATK_report':
         runpy.run_module('omics_pipe.WES_GATK_report', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'WGS_GATK_group_calling':
         runpy.run_module('omics_pipe.WGS_GATK_group_calling', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'SomaticInDels':
         runpy.run_module('omics_pipe.SomaticInDels', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'ChIPseq_MACS':
         runpy.run_module('omics_pipe.ChIPseq_MACS', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'ChIPseq_HOMER':
         runpy.run_module('omics_pipe.ChIPseq_HOMER', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'TCGA_download':
         runpy.run_module('omics_pipe.TCGA_download', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'Variant_annotation':
         runpy.run_module('omics_pipe.Variant_annotation', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'WGS_GATK_optimized':
         runpy.run_module('omics_pipe.WGS_GATK_optimized', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'test':
         runpy.run_module('omics_pipe.test', run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
+        compress(p.DATA["RAW_DATA"], args.compression)
         sumatra_end(start_time, record, project)
         sys.exit(0)
     elif args.analysis_type == 'custom':
         os.chdir(path)
         sys.path.append(path)
         runpy.run_module(script, run_name="__main__", alter_sys = True)
-        compress(p.RAW_DATA_DIR, args.compression)
-        sumatra_end(start_time, record, project)
+        #compress(p.DATA["RAW_DATA"], args.compression)
+ #       sumatra_end(start_time, record, project)
         sys.exit(0)   
     else:
         print 'Error: unsupported analysis type. Please try again.'       
