bdaf6f7bad4616769626c6927037c2280e9b4741
meissnert <meissner.t@googlemail.com>
2016-09-09 21:21:19 +0000

    fixed typos

--- omics_pipe/modules/RNAseq_report_counts.py
+++ omics_pipe/modules/RNAseq_report_counts.py
@@ -25,7 +25,7 @@ def RNAseq_report_counts(sample, RNAseq_report_counts_flag):
         PARAMS_FILE:
         '''
     sample = sample + extension
-    spawn_job(jobname = 'RNAseq_report_counts', SAMPLE = sample, LOG_PATH = p.OMICSPIPE["LOG_PATH"], RESULTS_EMAIL = p.OMICSPIPE["EMAIL"], SCHEDULER = p.OMICSPIPE["SCHEDULER"], walltime = p.DIFF_DESEQ["WALLTIME"], queue = p.OMICSPIPE["QUEUE"], nodes = p.DIFF_DESEQ["NODES"], ppn = p.DIFF_DESEQ["CPU"], memory = p.DIFF_DESEQ["MEMORY"] script = "/RNAseq_report_counts.sh", args_list = [sample,p.OMICSPIPE["WORKING_DIR"],p.DIFF_DESEQ["R_VERSION"], p.DIFF_DESEQ["RESULTS"],p.OMICSPIPE["PARAMS_FILE"]])
+    spawn_job(jobname = 'RNAseq_report_counts', SAMPLE = sample, LOG_PATH = p.OMICSPIPE["LOG_PATH"], RESULTS_EMAIL = p.OMICSPIPE["EMAIL"], SCHEDULER = p.OMICSPIPE["SCHEDULER"], walltime = p.DIFF_DESEQ["WALLTIME"], queue = p.OMICSPIPE["QUEUE"], nodes = p.DIFF_DESEQ["NODES"], ppn = p.DIFF_DESEQ["CPU"], memory = p.DIFF_DESEQ["MEMORY"], script = "/RNAseq_report_counts.sh", args_list = [sample,p.OMICSPIPE["WORKING_DIR"],p.DIFF_DESEQ["R_VERSION"], p.DIFF_DESEQ["RESULTS"],p.OMICSPIPE["PARAMS_FILE"]])
     job_status(jobname = 'RNAseq_report_counts', resultspath = p.DIFF_DESEQ["RESULTS"], SAMPLE = sample, outputfilename = sample + "/" + sample + ".html", FLAG_PATH = p.FLAG_PATH)
     return
     
